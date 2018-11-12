3046bdccedae6a4dca53343ad17c9a29f851c6c0
Lucas Breur <lucasbreur@gmail.com>
2018-11-12 15:52:13 -0200

    Added the win scene to the game

---
+++ game/scenes/winScene.h
@@ -0,0 +1,16 @@
+//
+// SENAC BCC PI 2
+// Projeto Betelgeuse
+//
+// criminosoPreso.h
+// estruturas da cena do criminoso preso
+//
+#ifndef betelgeuse_criminoso_preso_scene_h
+#define betelgeuse_criminoso_preso_scene_h
+
+#include "../engine/engine.h"
+
+Scene makeWinScene(Game *game);
+
+#endif
+
