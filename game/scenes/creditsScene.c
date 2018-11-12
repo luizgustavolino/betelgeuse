3a20a763aa0d9f49c2d947f0dc89f7813ebb38b8
vinyciusmz <zanardimz@gmail.com>
2018-11-12 20:53:29 -0200

    Add files via upload
    Adicionando a cena de créditos.

---
+++ game/scenes/creditsScene.c
@@ -0,0 +1,39 @@
+//
+// SENAC BCC PI 2
+// Projeto Betelgeuse
+
+#include <stddef.h>
+#include <stdio.h>
+#include "../engine/colors.h"
+#include "creditsScene.h"
+#include "mainMenuScene.h"
+
+static int senacLogo, senacTitle;
+static int bccLogo, bccTitle;
+static int betelgeuseLogo;
+
+static void creditsOnEnter(Game *game, int frame) {
+
+	senacLogo  = loadImageAsset("splash_senac_logo.png");
+}
+
+static void creditsOnFrame(Game *game, int frame) {
+
+
+	if (frame == 1){
+		fillRGB(game, BLACK);
+        drawImageAsset(senacLogo,100,100);
+	}
+}
+
+static void creditsOnExit(Game *game, int frame) {
+	unloadImageAsset(senacLogo);
+}
+
+Scene makeCreditsScene(Game *game) {
+	Scene credits;
+	credits.onEnter = creditsOnEnter;
+	credits.onFrame = creditsOnFrame;
+	credits.onExit  = creditsOnExit;
+	return credits;
+}
---
+++ game/scenes/creditsScene.h
@@ -0,0 +1,14 @@
+// SENAC BCC PI 2
+// Projeto Betelgeuse
+//
+// creditsScene.h
+// estruturas da Cena de Créditos
+//
+#ifndef betelgeuse_credits_scene_h
+#define betelgeuse_credits_scene_h
+
+#include "../engine/engine.h"
+
+Scene makeCreditsScene(Game *game);
+
+#endif
