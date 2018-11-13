103addac667e89f32998c9cfba3cf0ddee8b4335
vinyciusmz <zanardimz@gmail.com>
2018-11-13 12:45:59 -0200

    Credits Scene linked to Main Menu

--- game/scenes/mainMenuScene.c
+++ game/scenes/mainMenuScene.c
@@ -7,6 +7,7 @@
 #include "mainMenuScene.h"
 #include "splashScene.h"
 #include "reportScene.h"
+#include "creditsScene.h"
 
 // Forward declarations
 char* menuOptionsDatasource(Game *game, int option);
@@ -83,6 +84,7 @@ void menuCallback(Game *game, int choosenOption) {
 			changeScene(game, makeReportScene(game));
 			break;
 		case MAIN_MENU_OPT_CREDITS:
+            changeScene(game, makeCreditsScene(game));
 			break;
 		default:
 			break;
