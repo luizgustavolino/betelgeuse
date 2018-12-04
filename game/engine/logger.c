96bf5fed3bd8d552de8d19c32251c48b96c6fbd9
Lino <lgustavo@enjoei.com.br>
2018-12-03 23:18:01 -0200

    removing log on release version

--- game/engine/logger.c
+++ game/engine/logger.c
@@ -6,40 +6,50 @@
 #include <stdarg.h>
 #include "logger.h"
 
+#define LOGGING 0
+
 static void logInfo(const char *format, ...) {
-	fprintf(stdout, "\033[32m[info]\033[0m ");
-	va_list arg;
-	va_start (arg, format);
-   	vfprintf (stdout, format, arg);
-   	va_end (arg);
-   	fprintf(stdout, "\n");
+	if (LOGGING) {
+		fprintf(stdout, "\033[32m[info]\033[0m ");
+		va_list arg;
+		va_start (arg, format);
+   		vfprintf (stdout, format, arg);
+   		va_end (arg);
+   		fprintf(stdout, "\n");
+   	}
 }
 
 static void logWarning(const char *format, ...) {
-	fprintf(stdout, "\033[33m[warning]\033[0m ");
-	va_list arg;
-	va_start (arg, format);
-   	vfprintf (stdout, format, arg);
-   	va_end (arg);
-   	fprintf(stdout, "\n");
+	if (LOGGING) {
+		fprintf(stdout, "\033[33m[warning]\033[0m ");
+		va_list arg;
+		va_start (arg, format);
+   		vfprintf (stdout, format, arg);
+   		va_end (arg);
+   		fprintf(stdout, "\n");
+   	}
 }
 
 static void logError(const char *format, ...) {
-	fprintf(stdout, "\033[31m[error]\033[0m ");
-	va_list arg;
-	va_start (arg, format);
-   	vfprintf (stdout, format, arg);
-   	va_end (arg);
-   	fprintf(stdout, "\n");
+	if (LOGGING) {
+		fprintf(stdout, "\033[31m[error]\033[0m ");
+		va_list arg;
+		va_start (arg, format);
+   		vfprintf (stdout, format, arg);
+   		va_end (arg);
+   		fprintf(stdout, "\n");
+   	}
 }
 
 static void logComplement(const char *format, ...) {
-	fprintf(stdout, "\t⮑  ");
-	va_list arg;
-	va_start (arg, format);
-   	vfprintf (stdout, format, arg);
-   	va_end (arg);
-   	fprintf(stdout, "\n");
+	if (LOGGING) {
+		fprintf(stdout, "\t⮑  ");
+		va_list arg;
+		va_start (arg, format);
+   		vfprintf (stdout, format, arg);
+   		va_end (arg);
+   		fprintf(stdout, "\n");
+   	}
 }
 
 void setupLogger(){
